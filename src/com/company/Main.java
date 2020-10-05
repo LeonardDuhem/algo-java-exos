29a0563696cb04e65e0f6e2480798a5303f481ca
leonard duhem <leonard.duhem@gmail.com>
2020-10-05 16:17:16 +0200

    Tableau

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] scores = new int[7];
+        scores [0] = 27 ;
+        scores [1] = 1 ;
+        scores [2] = 15 ;
+        scores [3] = 23 ;
+        scores [4] = 46 ;
+        scores [5] = 32 ;
+        scores [6] = 50 ;
+        for (int i = 0 ; i < scores.length ; i++ ) {
+            int s = scores [i] ;
+            System.out.println("scores = " + s );
+        }
+        int somme = 0 ;
+        for(int i = 0 ; i < scores.length ; i++) {
+            somme += scores[i];
+        }
+        float moyenne = somme / scores.length ;
+        System.out.println("la moyenne des scores est : " + moyenne);
+
+
+        
+
+
+
+
     }
 }
